904f02201fae6592866cd6d9480f206c70cad910
Wojciech Buńka <wojtek.bunka@gmail.com>
2021-02-24 11:57:46 +0100

    Add some more projects but now with correct version

---
+++ ListaZakupow.EF/ListaZakupowContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaZakupow.EF
+{
+    public class ListaZakupowContext : DbContext
+    {
+        public ListaZakupowContext(): base()
+        {
+
+        }
+    }
+}
---
+++ ListaZakupow.Logic/Logic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaZakupow.Logic
+{
+    class Logic
+    {
+    }
+}
